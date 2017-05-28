9e16b8c2ebe2ad2e08657ad059189681ada648a4
Angel Wu <PMheart@users.noreply.github.com>
2017-05-28 11:11:45 +0800

    typo

--- CoreDisplayFixup/OSMinorVersion.cpp
+++ CoreDisplayFixup/OSMinorVersion.cpp
@@ -14,12 +14,13 @@ int OSVersion::getMinorVersion() {
 	// Reference:
 	// https://en.wikipedia.org/wiki/MacOS_Sierra#Releases
 	//
-	// Apple has reversed Darwin 16.2.0 due to unknown reasons,
+	// Apple has reserved Darwin 16.2.0 due to unknown reasons,
 	// and thus we can't get the real macOS version,
 	// so here we introduce a specific algo to do that.
-	if (version_major == 16 && version_minor > 2)
+	//
+	if (version_major == 16 && version_minor > 2) // 10.12.2+
 		this->minor = version_minor - 1;
-	else
+	else	// legacy versions
 		this->minor = version_minor;
 	
 	return this->minor;
