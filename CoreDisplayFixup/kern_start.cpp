5205b72cc7999c8980fad66d2042ba89e0075188
Vanilla <PMheart@users.noreply.github.com>
2017-12-28 19:29:43 +0800

    Fix indentations

--- CoreDisplayFixup/kern_start.cpp
+++ CoreDisplayFixup/kern_start.cpp
@@ -21,9 +21,9 @@ static void intelPatcherStart()
 	if (getKernelVersion() == KernelVersion::Yosemite || getKernelVersion() == KernelVersion::ElCapitan) // 10.10, 10.11
 		lilu.onProcLoad(ADDPR(procInfoYosEC), ADDPR(procInfoSize), nullptr, nullptr, ADDPR(binaryModYosEC), ADDPR(binaryModSize));
 	else if (getKernelVersion() == KernelVersion::Sierra || (getKernelVersion() == KernelVersion::HighSierra && getKernelMinorVersion() < 3)) // 10.12 - 10.13.1
-    lilu.onProcLoad(ADDPR(procInfoSieHS), ADDPR(procInfoSize), nullptr, nullptr, ADDPR(binaryModSieHS), ADDPR(binaryModSize));
-  else if (getKernelVersion() == KernelVersion::HighSierra && getKernelMinorVersion() >= 3) // 10.13.2+
-    lilu.onProcLoad(ADDPR(procInfoSieHS), ADDPR(procInfoSize), nullptr, nullptr, ADDPR(binaryModHS10132), ADDPR(binaryModSize));
+		lilu.onProcLoad(ADDPR(procInfoSieHS), ADDPR(procInfoSize), nullptr, nullptr, ADDPR(binaryModSieHS), ADDPR(binaryModSize));
+	else if (getKernelVersion() == KernelVersion::HighSierra && getKernelMinorVersion() >= 3) // 10.13.2+
+		lilu.onProcLoad(ADDPR(procInfoSieHS), ADDPR(procInfoSize), nullptr, nullptr, ADDPR(binaryModHS10132), ADDPR(binaryModSize));
 
 }
 
